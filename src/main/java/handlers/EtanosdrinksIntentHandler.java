9a47bcffdd856eddcf197cd371a57ace38093514
Tuere Pinto <tuere.pinto@bancointer.com.br>
2021-02-28 13:11:28 -0300

    Eita nos drinks

--- src/main/java/handlers/EtanosdrinksIntentHandler.java
+++ src/main/java/handlers/EtanosdrinksIntentHandler.java
@@ -7,7 +7,7 @@ import com.amazon.ask.request.Predicates;
 
 import java.util.Optional;
 
-public class PodcastIntentHandler implements RequestHandler {
+public class EtanosdrinksIntentHandler implements RequestHandler {
     @Override
     public boolean canHandle(HandlerInput input) {
         //TODO Criar Intet dentro console developer Amazon
@@ -18,14 +18,17 @@ public class PodcastIntentHandler implements RequestHandler {
     public Optional<Response> handle(HandlerInput input) {
 
         //TODO Colocar texto
-        String speechText = "<p>Que tal experimentar o <lang xml:lang=\"en-US\">podcast</lang> PodProgramar?</p>" +
-                "<p>É um <lang xml:lang=\"en-US\">podcast</lang> apresentado por desenvolvedoras focado em programação, " +
-                "notícias e histórias, tudo com o toque feminino, e com bastante diversidade.</p>" +
-                "<p>Tente dizer Alexa, abra o <lang xml:lang=\"en-US\">Spotify</lang> podprogramar.</p>";
+        String speechText = "<p>Que tal pedir aquela cerveja no, Eta nois drinks?</p>" +
+                "<p>Eta nois drinks é especializada em bebidas destiladas e cervejas.</p>" +
+                "<p>E o melhor, você pode pedir pelo whatsapp. As entregas são agendadas, " +
+                "seguem os protocolos contra COVID-19 </p>" +
+                "<p>Quer saber mais? Faz aquele zap zap para o número (31) 9 9 1 5 8-3 4 0 4</p>";
+
+        String screenText = "Instagran: @etanoisdrinks\r\nWhatsapp: (31) 99158-3404";
 
         return input.getResponseBuilder()
                 .withSpeech(speechText)
-                .withSimpleCard("PodProgramar", speechText)
+                .withSimpleCard("PodProgramar", screenText)
                 .withShouldEndSession(false)
                 .build();
     }
